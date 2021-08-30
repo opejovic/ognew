df0688c73c4bd7e54626df7573c94f19c3b76d3f
Ognjen <opejovic@gmail.com>
2021-08-30 16:46:50 +0200

    inital commit

---
+++ src/views/AppHome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <div class="home__copy">
+      <h1 class="home__heading">
+        Hello I’m Ognjen. <br />I build things for the web.
+      </h1>
+      <p class="home__description">
+        I’m a freelancer specialising in website development, digital design, UX and UI, using <strong>Vue.js</strong>, <strong>Laravel</strong> and <strong>Figma</strong>. I know my name can be hard to pronounce for some people - so, feel free call me Og.
+      </p>
+    </div>
+
+    <div class="home__right">
+      <div class="home__blob">
+        <img src="../assets/blob_landing.svg" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+});
+</script>
---
+++ src/views/AppWork.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="work">
+    <section class="work__process">
+      <h1 class="work__heading">The process</h1>
+      <div class="work__process__discover">
+        <h2 class="process-title proces-title__discover">01 discover</h2>
+        <ul class="process-list">
+          <li class="process-list-item">Listen</li>
+          <li class="process-list-item">Question</li>
+          <li class="process-list-item">Collaborate</li>
+          <li class="process-list-item">Research</li>
+          <li class="process-list-item">Prototype</li>
+        </ul>
+      </div>
+      <div class="work__process__design">
+        <h2 class="process-title proces-title__design">02 design</h2>
+        <ul class="process-list">
+          <li class="process-list-item">Digital</li>
+          <li class="process-list-item">UX / UI</li>
+          <li class="process-list-item">Print</li>
+          <li class="process-list-item">Motion</li>
+          <li class="process-list-item">Illustration</li>
+        </ul>
+      </div>
+      <div class="work__process__develop">
+        <h2 class="process-title proces-title__develop">03 develop</h2>
+        <ul class="process-list">
+          <li class="process-list-item">Full-stack</li>
+          <li class="process-list-item">Accessibility</li>
+          <li class="process-list-item">Ecommerce</li>
+          <li class="process-list-item">SEO / QA</li>
+          <li class="process-list-item">Cloud Hosting</li>
+        </ul>
+      </div>
+
+       <div class="work__blob">
+        <img src="../assets/blob_work.svg" alt="">
+      </div>
+    </section>
+
+
+  </div>
+</template>
